51adc40e3007a1c5f1be8dd3d69552662b7b05b4
Brett Dale <brettdale828@gmail.com>
2021-03-19 17:08:55 -0400

    forgot which $ is file

--- valid.sh
+++ valid.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+# Shell script to make sure file created is valid
+
+# Author: Carlee Yancey
+# Author: Brett Dale
+# Version: 1 (March 24, 2021)
+
+# Exit Status:
+#   exit 0: Good
+#   exit 1: usage error dealing with args
+#   exit 2: file name already exist
+#   exit 3: Length of state is not 2
+
+#first step is to check permissions of file passed in
+filename=$1;
+
+echo "$filename";
